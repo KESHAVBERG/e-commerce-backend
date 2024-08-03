f397d218020110c8d8eba4094f5a9dfd2bec7c16
KESHAVBERG <kingkeshav7@gmail.com>
2024-08-03 20:08:53 +0530

    implemented get all users features

--- controller/admincoller.js
+++ controller/admincoller.js
@@ -1,15 +1,25 @@
 const userModel = require("../models/user");
 const asyncHandler = require("express-async-handler");
 
-
 // find user
 
-exports.findUser = asyncHandler(async(req, res)=>{
-        try{
-            const user = await userModel.findById({_id:req.params.id});
-            return res.status(203).send(user);
-        }catch(e){
-            return res.status(500).send({err:e});
-        }
-    }
-);
+exports.findUser = asyncHandler(async (req, res) => {
+  try {
+    const user = await userModel.findById({ _id: req.params.id });
+    const { password, ...others } = user._doc;
+    return res.status(203).send({ ...others });
+  } catch (e) {
+    return res.status(500).send({ err: e });
+  }
+});
+
+// Find All users
+
+exports.getAllUsers = asyncHandler(async (req, res) => {
+  try {
+    const users = await userModel.find();
+    res.status(200).send(users);
+  } catch (e) {
+    res.status(500).send({ erro: e });
+  }
+});
--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,9 @@
-const { findUser } = require("../controller/admincoller");
+const { findUser, getAllUsers } = require("../controller/admincoller");
 const { verifyTokenAndAdmin } = require("../middlewear/verifyToken");
 
 const router = require("express").Router();
 
 router.get("/finduser/:id", verifyTokenAndAdmin, findUser);
+router.get("/getAllUsers", verifyTokenAndAdmin, getAllUsers);
+
 module.exports = router;
